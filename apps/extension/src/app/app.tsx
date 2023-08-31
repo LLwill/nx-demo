bc9b9df1590abcf9a84c0744a082e5db2fae6e6d
will <willzhiwei@163.com>
2023-09-01 01:09:04 +0800

    fix: tsconfig妥协(⊙︿⊙)

--- apps/extension/src/app/app.tsx
+++ apps/extension/src/app/app.tsx
@@ -7,6 +7,7 @@ import NxWelcome from './nx-welcome';
 import { Route, Routes, Link } from 'react-router-dom';
 import Browser from 'webextension-polyfill';
 import { MSG_OPEN_MAIN } from '@/constants';
+import { Home } from '@nx-demo/containers';
 
 export function App() {
   console.log('😄😄😄插件测试！！！加载App~');
@@ -26,44 +27,7 @@ export function App() {
   }, [mainVisiable]);
   return mainVisiable ? (
     <div className={styles.main}>
-      <NxWelcome title="extension" />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
+      <Home />
     </div>
   ) : (
     <></>
