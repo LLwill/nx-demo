c172484a63f6532a92543fd089c3da6acfe3bf29
will <willzhiwei@163.com>
2023-09-05 14:42:20 +0800

    feat: 新建sse测试服务

---
+++ apps/sse-demo-api/src/main.ts
@@ -0,0 +1,40 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import express from 'express';
+import * as path from 'path';
+
+const app = express();
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/api', (req, res) => {
+  res.send({ message: 'Welcome to sse-demo-api!' });
+});
+
+app.post('/sse', (req, res) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+  res.flushHeaders();
+
+  let counter = 0;
+  const interval = setInterval(() => {
+    const eventData = `data: This is event ${counter}\n\n`;
+    res.write(eventData);
+    counter++;
+
+    if (counter === 10) {
+      clearInterval(interval);
+      res.end();
+    }
+  }, 1000);
+});
+
+const port = process.env.PORT || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
